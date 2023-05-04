ce1ead36a499607c8093fcc1f696af7db9d54e18
Rafi Fajar Sulaiman <rafihariyadi01@gmail.com>
2023-05-04 22:30:43 +0700

    fixed small bugs on search bar

--- public/js/searchAjax.js
+++ public/js/searchAjax.js
@@ -5,13 +5,13 @@ $(document).ready(function(){
   $('#state').val('');
   var searchField = $('#search').val();
   var expression = new RegExp(searchField, "i");
-  $.getJSON('demo.json', function(data) {
+  $.getJSON('/demo.json', function(data) {
     console.log('dapet akses')
    $.each(data, function(key, value){
-    if (value.nama.search(expression) != -1 || value.pengarang.search(expression) != -1)
-    {
-        console.log('dapet akses 2')
-     $('#result').append('<li class="list-group-item link-class"><img src="'+value.gambar+'" height="40" width="40" class="img-thumbnail" /> '+value.nama+' | <span class="text-muted">'+value.pengarang+'</span></li>');
+    if (value.nama.search(expression) != -1 || value.pengarang.search(expression) != -1) {
+        $('#result').append(
+            '<li class="list-group-item link-class"><img src="'+value.gambar+'" height="40" width="40" class="img-thumbnail" /> '+value.nama+' | <span class="text-muted">'+value.pengarang+'</span></li>'
+            );
     }
    });   
   });
